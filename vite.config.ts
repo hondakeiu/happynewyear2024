bef9c4ce93d7d5a86a90cb56ea9a260c9d861be2
honda keiu <k-honda@studio-spoon.co.jp>
2024-01-02 00:59:16 +0900

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,19 @@ import path from 'path';
 import handlebars from 'vite-plugin-handlebars';
 
 export default defineConfig({
+  base: "/happynewyear2024/",
   plugins: [
     handlebars({
       partialDirectory: path.resolve(__dirname, './src/partials/'),
       context: {
         data: {
-          productionRoot: '',
+          productionRoot: 'https://hondakeiu.github.io/happynewyear2024/',
         },
       },
     }) as PluginOption,
   ],
   build: {
+    outDir: 'docs',
     rollupOptions: {
       input: {
         home: path.resolve(__dirname, './index.html'),
