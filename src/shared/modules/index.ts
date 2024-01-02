bfbc37c07fb328557b6f49db5f7592e47b560aa0
honda keiu <k-honda@studio-spoon.co.jp>
2024-01-02 23:11:10 +0900

    track4

--- src/shared/modules/index.ts
+++ src/shared/modules/index.ts
@@ -3,7 +3,7 @@ import { Howl } from 'howler';
 let player: Howl | null = null;
 let pausedTime = 0;
 let currentTrack = 1;
-const volumes = [0.5, 0.4, 0.4];
+const volumes = [0.45, 0.4, 0.35, 0.35];
 
 const play = () => {
   if (player) {
@@ -13,14 +13,14 @@ const play = () => {
     src: [`assets/sounds/track${currentTrack}.mp3`],
     volume: volumes[currentTrack - 1],
     onload: () => {
-      home.classList.remove('is-track1', 'is-track2', 'is-track3');
+      home.classList.remove('is-track1', 'is-track2', 'is-track3', 'is-track4');
       home.classList.add(`is-track${currentTrack}`);
       player?.play();
       player?.seek(pausedTime);
     },
     onend: () => {
       home.classList.remove(`is-track${currentTrack}`);
-      currentTrack = currentTrack % 3 + 1;
+      currentTrack = currentTrack % 4 + 1;
       pausedTime = 0;
       play();
     },
@@ -52,7 +52,7 @@ playButton.addEventListener('click', () => {
 
 backButton.addEventListener('click', () => {
   if (player && player.playing()) {
-    currentTrack = currentTrack === 1 ? 3 : currentTrack - 1;
+    currentTrack = currentTrack === 1 ? 4 : currentTrack - 1;
     pausedTime = 0;
     player.stop();
     play();
@@ -61,7 +61,7 @@ backButton.addEventListener('click', () => {
 
 skipButton.addEventListener('click', () => {
   if (player && player.playing()) {
-    currentTrack = currentTrack % 3 + 1;
+    currentTrack = currentTrack % 4 + 1;
     pausedTime = 0;
     player.stop();
     play();
